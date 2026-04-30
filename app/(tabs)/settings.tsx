5f49d574bb9dac14bc407aafb5a79b73b35e6105
Braide Daniella Biobele <dannygodwin2003@gmail.com>
2026-04-30 15:41:07 +0100

    Update to my tracker app

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,110 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Add from "../components/add";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Settings() {
   return (
     <SafeAreaView style={style.container}>
-      <Text>Settings</Text>
-      <Add/>
+      {/* profile card */}
+      <View></View>
+      {/* account section */}
+      <View>
+        <Text>Account</Text>
+        {/* account card */}
+        <View>
+          {/* personal information */}
+          <View>
+            <View>
+              <View>
+                <Ionicons name="person" />
+              </View>
+              <View>
+                <Text>Personal Information</Text>
+                <Text>Update your details</Text>
+              </View>
+            </View>
+            <Ionicons name="chevron-forward-outline" />
+          </View>
+          {/* security */}
+          <View>
+            <View>
+              <View>
+                <Ionicons />
+              </View>
+              <View>
+                <Text>Personal Information</Text>
+                <Text>Update your details</Text>
+              </View>
+            </View>
+            <Ionicons />
+          </View>
+        </View>
+      </View>
+      {/* notification section */}
+      <View>
+        <Text>Notifications</Text>
+        {/* notifications card */}
+        <View>
+          <View>
+            <View>
+              <Ionicons name="person" />
+            </View>
+            <View>
+              <Text>Personal Information</Text>
+              <Text>Update your details</Text>
+            </View>
+          </View>
+          <Ionicons name="chevron-forward-outline" />
+        </View>
+      </View>
+      {/* app preference section */}
+      <View>
+        <Text>App preferences</Text>
+        {/* app preference card */}
+        <View>
+          {/* dark mode */}
+          <View>
+            <View>
+              <View>
+                <Ionicons name="person" />
+              </View>
+              <View>
+                <Text>Personal Information</Text>
+                <Text>Update your details</Text>
+              </View>
+            </View>
+            <Ionicons name="chevron-forward-outline" />
+          </View>
+          {/* language */}
+          <View>
+            <View>
+              <View>
+                <Ionicons />
+              </View>
+              <View>
+                <Text>Personal Information</Text>
+                <Text>Update your details</Text>
+              </View>
+            </View>
+            <Ionicons />
+          </View>
+          {/* signout */}
+          <View>
+            <View>
+              <View>
+                <Ionicons />
+              </View>
+              <View>
+                <Text>Personal Information</Text>
+                <Text>Update your details</Text>
+              </View>
+            </View>
+            <Ionicons />
+          </View>
+        </View>
+      </View>
+      <Add />
     </SafeAreaView>
   );
 }
@@ -16,5 +114,10 @@ const style = StyleSheet.create({
     flex: 1,
     // justifyContent: 'center',
     // alignItems: 'center'
-  }
-})
+  },
+  card: {
+    borderRadius: 25,
+    padding: 20,
+    gap: 10,
+  },
+});
--- app/components/add.tsx
+++ app/components/add.tsx
@@ -1,20 +1,73 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  Modal,
+  SectionList,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Add() {
+  const [visible, setVisible] = useState(false);
   return (
-    <View style={{
-      position: "absolute",
-      bottom: 20,
-      right: 20,
-      backgroundColor: "#007B83",
-      width: 50,
-      height: 50,
-      borderRadius: 30,
-      justifyContent: "center",
-      alignItems: 'center',
-    }}>
-      <Ionicons name="add" size={20} color={'white'}/>
-    </View>
-  )
+    <SafeAreaView>
+      <View
+        style={{
+          position: "absolute",
+          bottom: 20,
+          right: 20,
+          backgroundColor: "#007B83",
+          width: 50,
+          height: 50,
+          borderRadius: 30,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Ionicons
+          name="add"
+          size={20}
+          color={"white"}
+          onPress={() => setVisible(true)}
+        />
+      </View>
+
+      <Modal
+        visible={visible}
+        animationType="slide"
+        onRequestClose={() => setVisible(false)}
+      >
+        <View>
+          <Button title="X" onPress={() => setVisible(false)} />
+          <Text>Add Item</Text>
+        </View>
+        <View>
+          <View>
+            <Text>Task title</Text>
+            <TextInput placeholder="Input the text title here..." />
+          </View>
+          <View>
+            <Text>Categories</Text>
+            <View>
+              <View>
+                <Ionicons icon={"briefcase"} />
+                <Text>Work</Text>
+              </View>
+            </View>
+          </View>
+          <View>
+            <Text>Notes & details</Text>
+            <TextInput placeholder="write every important note details here..." />
+          </View>
+          <View>
+            <Text>Deadline date</Text>
+            
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
 }
